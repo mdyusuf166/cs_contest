c5fd3ff071e391fbe3c43e0f66132ce00d30d0b9
Md Yousuf <yousuf381256@gmail.com>
2026-06-03 20:27:10 +0600

    s

--- 62.cpp
+++ 62.cpp
@@ -1,14 +1,66 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void solve() {
+//     int n;
+//     cin >> n;
+
+//     vector<int> a(n);
+
+//     for (int i = 0; i < n; i++) {
+//         cin >> a[i];
+//     }
+
+//     int sum = a[0] + a[n - 1];
+
+//     bool ok = true;
+
+//     for (int i = 0; i < n / 2; i++) {
+//         if (a[i] + a[n - 1 - i] != sum) {
+//             ok = false;
+//         }
+//     }
+
+//     if (ok) {
+//         cout << "Yes\n";
+//     }
+//     else {
+//         cout << "No\n";
+//     }
+// }
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(0);
+
+//     int t;
+//     cin >> t;
+
+//     while (t--) {
+//         solve();
+//     }
+
+//     return 0;
+// // }
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
+#define YOUSUF ios::sync_with_stdio(false); cin.tie(0);
+#define BioTech cin
+#define Cortex cout
+#define NeuroQuantum vector<int>
+#define Genesis return
+
 void solve() {
     int n;
-    cin >> n;
+    BioTech >> n;
 
-    vector<int> a(n);
+    NeuroQuantum a(n);
 
     for (int i = 0; i < n; i++) {
-        cin >> a[i];
+        BioTech >> a[i];
     }
 
     int sum = a[0] + a[n - 1];
@@ -22,23 +74,22 @@ void solve() {
     }
 
     if (ok) {
-        cout << "Yes\n";
+        Cortex << "Yes\n";
     }
     else {
-        cout << "No\n";
+        Cortex << "No\n";
     }
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+    YOUSUF
 
     int t;
-    cin >> t;
+    BioTech >> t;
 
     while (t--) {
         solve();
     }
 
-    return 0;
+    Genesis 0;
 }
